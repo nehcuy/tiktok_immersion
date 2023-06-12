5812dbe4f69bb0251406ae2c4d178b3401d24111
yuchen <hyuchen2607@gmail.com>
2023-06-12 23:23:34 +0800

    feat: set up proper pull response

--- rpc-server/db.go
+++ rpc-server/db.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"strings"
-	"fmt"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 	_ "github.com/lib/pq"
@@ -24,7 +24,7 @@ func NewDatabase() (*Database, error) {
 		log.Fatalf("Failed to open database connection: %v", err)
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS messages (chat TEXT PRIMARY KEY, sender TEXT, text TEXT, send_time BIGINT);")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS messages (chat TEXT, sender TEXT, text TEXT, send_time BIGINT PRIMARY KEY);")
 	if err != nil {
 		log.Fatalf("Failed to create table: %v", err)
 	}
@@ -35,6 +35,49 @@ func NewDatabase() (*Database, error) {
 }
 
 func (db *Database) InsertMessage(chat string, sender string, text string, send_time int64) error {
+	chat = db.ReformatChat(chat)
+
+	insertQuery := fmt.Sprintf(
+		"INSERT INTO messages (chat, sender, text, send_time) VALUES ('%s', '%s', '%s', %d)",
+		chat, sender, text, send_time,
+	)
+
+	_, err := db.connection.Exec(insertQuery)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *Database) GetMessages(chat string) ([]*Message, error) {
+	chat = db.ReformatChat(chat)
+	var (
+		rows     *sql.Rows
+		messages []*Message
+		err      error
+	)
+
+	selectQuery := fmt.Sprintf(
+		"SELECT * FROM messages WHERE chat='%s' ORDER BY send_time ASC", chat,
+	)
+
+	rows, err = db.connection.Query(selectQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		temp := &Message{}
+		err := rows.Scan(&temp.Chat, &temp.Sender, &temp.Text, &temp.SendTime)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, temp)
+	}
+	return messages, nil
+}
+
+func (db *Database) ReformatChat(chat string) string {
 	// Read the chat of message and sort it before inserting
 	// For instance, {A, B} and {B, A} are the same
 	chatList := strings.Split(chat, ":")
@@ -46,15 +89,5 @@ func (db *Database) InsertMessage(chat string, sender string, text string, send_
 	} else {
 		new_chat = name1 + ":" + name2
 	}
-	
-	insertQuery := fmt.Sprintf(
-		"INSERT INTO messages (chat, sender, text, send_time) VALUES ('%s', '%s', '%s', %d)",
-		new_chat, sender, text, send_time,
-	)
-
-	_, err := db.connection.Exec(insertQuery)
-	if err != nil {
-		return err
-	}
-	return nil
+	return new_chat
 }
--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"strconv"
+	"fmt"
+	"time"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
@@ -15,11 +16,14 @@ type IMServiceImpl struct {
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
 	resp := rpc.NewSendResponse()
 	resp.SetCode(0)
+	resp.SetMsg("success")
+
 	chat := req.GetMessage().GetChat()
 	sender := req.GetMessage().GetSender()
 	text := req.GetMessage().GetText()
 	time := req.GetMessage().GetSendTime()
-	resp.SetMsg(prettyPrint(chat, sender, text, time))
+
+	fmt.Println(prettyPrint(chat, sender, text, time))
 
 	err := s.db.InsertMessage(chat, sender, text, time)
 	if err != nil {
@@ -31,10 +35,34 @@ func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.Se
 }
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+	chat := req.GetChat()
+	messages, err := s.db.GetMessages(chat)
+	if err != nil {
+		return nil, err
+	}
+
+	respMessages := make([]*rpc.Message, 0)
+	for _, message := range messages {
+		temp := &rpc.Message{
+			Chat:     s.db.ReformatChat(chat),
+			Sender:   message.Sender,
+			Text:     message.Text,
+			SendTime: message.SendTime,
+		}
+		respMessages = append(respMessages, temp)
+		fmt.Println(prettyPrint(chat, message.Sender, message.Text, message.SendTime))
+	}
 	resp := rpc.NewPullResponse()
+	resp.SetCode(0)
+	resp.SetMsg("success")
+	resp.SetMessages(respMessages)
+
 	return resp, nil
 }
 
 func prettyPrint(chat string, sender string, text string, send_time int64) string {
-	return "{CHAT: " + chat + "; SENDER: " + sender + "; CONTENT: " + text + "; TIME: " + strconv.Itoa(int(send_time)) + "}"
+	t := time.Unix(send_time, 0)
+	pretty_time := t.Format("2006-01-02 15:04:05")
+	// split
+	return "{CHAT: " + chat + ";\nSENDER: " + sender + ";\nCONTENT: " + text + ";\nTIME: " + pretty_time + "}"
 }
